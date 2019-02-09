5b2829309c8567f343b9aa317c27361ec9a1662a
YamaSouichirou <souichirou19960210@gmail.com>
2019-02-10 00:06:39 +0900

    create table blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_085400) do
+ActiveRecord::Schema.define(version: 2019_02_09_085933) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "published_status", default: 10, null: false
+    t.date "publshed_on"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
